9648b793985b15b22d7ec3abb76712ed2a447e42
minhuy <forwork@minhuy.dev>
2026-05-18 17:32:25 +0700

    Limit ServBay paths to macOS

--- .config/zsh/20-path.zsh
+++ .config/zsh/20-path.zsh
@@ -1,8 +1,13 @@
-export PATH="/Applications/ServBay/package/python/current/Python.framework/Versions/Current/bin:$PATH"
+if [[ "$OSTYPE" == darwin* ]]; then
+  export PATH="/Applications/ServBay/package/python/current/Python.framework/Versions/Current/bin:$PATH"
+fi
+
 export PATH="$HOME/.local/bin:$PATH"
 
 export NVM_DIR="$HOME/.nvm"
 [[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
 [[ -s "$NVM_DIR/bash_completion" ]] && source "$NVM_DIR/bash_completion"
 
-export PATH="/Applications/ServBay/package/node/25/25.9.0/bin:$PATH"
+if [[ "$OSTYPE" == darwin* ]]; then
+  export PATH="/Applications/ServBay/package/node/25/25.9.0/bin:$PATH"
+fi
